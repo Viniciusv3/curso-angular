a88bbf45941262458fb910b004549df6960a48a9
Viniciusv3 <vinicius.jesus@sptech.school>
2023-03-27 11:23:17 -0300

    Eventos, Listagem, etc...

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { Animal } from './Animal';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent {
+
+  animals: Animal[] = [
+    {name: "Turca", type: "Dog", age: 4},
+    {name: "Tom", type: "Cat", age: 10},
+    {name: "Frida", type: "Dog", age: 5},
+    {name: "Bob", type: "Horse", age: 1},
+  ];
+
+  animal: Animal = {
+    name: 'teste',
+    type: 'Alguma coisa',
+    age: 10,
+  };
+
+  animalDetails = '';
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `o pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+}
